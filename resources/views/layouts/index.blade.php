59a83371ead1e93642d7c7c896e8f0fc66e014bb
Talahatu <87047371+Talahatu@users.noreply.github.com>
2023-07-09 23:18:13 +0700

    Fix maneh boi

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -202,7 +202,7 @@
 
         });
     </script>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    {{-- <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script> --}}
     @yield('script')
 </body>
 <!-- END BODY -->
